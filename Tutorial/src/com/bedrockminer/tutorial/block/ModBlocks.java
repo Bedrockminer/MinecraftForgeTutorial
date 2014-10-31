88cb5e165d475d9673950f16a8413c798b6377b6
Bedrockminer <minerbedrock@gmail.com>
2014-10-31 18:11:44 +0100

    Tutorial #11: Custom Food
    * Added ModItemFood and Texture for the chocolate Item.
    - Removed stuff from earlier tutorials
    
    Signed-off-by: Bedrockminer <minerbedrock@gmail.com>

--- Tutorial/src/com/bedrockminer/tutorial/block/ModBlocks.java
+++ Tutorial/src/com/bedrockminer/tutorial/block/ModBlocks.java
@@ -1,21 +1,9 @@
 package com.bedrockminer.tutorial.block;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
-
-import com.bedrockminer.tutorial.block.itemblock.ItemBlockMetaBlock;
-
-import cpw.mods.fml.common.registry.GameRegistry;
 
 public final class ModBlocks {
 
-	public static Block tutorialBlock;
-	public static Block multitexture;
-	public static Block metablock;
-
 	public static final void init() {
-		GameRegistry.registerBlock(tutorialBlock = new BasicBlock("tutorialBlock", Material.iron), "tutorialBlock");
-		GameRegistry.registerBlock(multitexture = new MultitextureBlock("multitexture", Material.cloth), "multitexture");
-		GameRegistry.registerBlock(metablock = new MetaBlock("metablock", Material.cloth), ItemBlockMetaBlock.class, "metablock");
+
 	}
 }
--- Tutorial/src/com/bedrockminer/tutorial/item/ModItems.java
+++ Tutorial/src/com/bedrockminer/tutorial/item/ModItems.java
@@ -1,47 +1,16 @@
 package com.bedrockminer.tutorial.item;
 
-import com.bedrockminer.tutorial.Main;
-
-import cpw.mods.fml.common.registry.GameRegistry;
-import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
-import net.minecraft.item.Item.ToolMaterial;
-import net.minecraft.item.ItemArmor.ArmorMaterial;
-import net.minecraftforge.common.util.EnumHelper;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import cpw.mods.fml.common.registry.GameRegistry;
+
 
 public final class ModItems {
 
-	public static ToolMaterial TUTORIAL = 	EnumHelper.addToolMaterial("TUTORIAL", 	3, 1000, 15.0F, 4.0F, 30);
-	public static ArmorMaterial ARMOR = 	EnumHelper.addArmorMaterial("TUTORIAL", 16, new int[] {3, 8, 6, 3}, 30);
-
-	public static Item tutorialItem;
-	public static Item metaitem;
-
-	public static Item tutorialPickaxe;
-	public static Item tutorialAxe;
-	public static Item tutorialSpade;
-	public static Item tutorialHoe;
-	public static Item tutorialSword;
-
-	public static Item tutorialHelmet;
-	public static Item tutorialChestplate;
-	public static Item tutorialLeggings;
-	public static Item tutorialBoots;
+	public static Item chocolate;
 
 	public static final void init() {
-		GameRegistry.registerItem(tutorialItem = new Item().setUnlocalizedName("tutorialItem").setCreativeTab(CreativeTabs.tabMisc).setTextureName(Main.MODID + ":tutorialItem"), "tutorialItem");
-		GameRegistry.registerItem(metaitem = new MetaItem("metaitem"), "metaitem");
-
-		GameRegistry.registerItem(tutorialPickaxe	= new ItemModPickaxe("tutorial_pickaxe", TUTORIAL), "tutorial_pickaxe");
-		GameRegistry.registerItem(tutorialAxe 		= new ItemModAxe("tutorial_axe", TUTORIAL), "tutorial_axe");
-		GameRegistry.registerItem(tutorialSpade 	= new ItemModSpade("tutorial_spade", TUTORIAL), "tutorial_spade");
-		GameRegistry.registerItem(tutorialHoe	 	= new ItemModHoe("tutorial_hoe", TUTORIAL), "tutorial_hoe");
-		GameRegistry.registerItem(tutorialSword 	= new ItemModSword("tutorial_sword", TUTORIAL), "tutorial_sword");
-
-		//Armor
-		GameRegistry.registerItem(tutorialHelmet	= new ItemModArmor("tutorial_helmet", ARMOR, "tutorial", 0), "tutorial_helmet");
-		GameRegistry.registerItem(tutorialChestplate= new ItemModArmor("tutorial_chestplate", ARMOR, "tutorial", 1), "tutorial_chestplate");
-		GameRegistry.registerItem(tutorialLeggings	= new ItemModArmor("tutorial_leggings", ARMOR, "tutorial", 2), "tutorial_leggings");
-		GameRegistry.registerItem(tutorialBoots		= new ItemModArmor("tutorial_boots", ARMOR, "tutorial", 3), "tutorial_boots");
+		GameRegistry.registerItem(chocolate = new ModItemFood("chocolate", 2, 0.2f, false, new PotionEffect(Potion.moveSpeed.id, 1200, 1), new PotionEffect(Potion.jump.id, 600, 0), new PotionEffect(Potion.regeneration.id, 200, 1)).setAlwaysEdible(), "chocolate");
 	}
 }
