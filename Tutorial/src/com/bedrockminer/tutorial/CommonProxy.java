f9c570257f36c57c5c411eb74ed8941999d753d0
Bedrockminer <minerbedrock@gmail.com>
2014-11-09 16:03:49 +0100

    Tutorial #13: World Generation (Ores)
    * Added World Generator for Ores from the Tutorial
    
    Signed-off-by: Bedrockminer <minerbedrock@gmail.com>

--- Tutorial/src/com/bedrockminer/tutorial/CommonProxy.java
+++ Tutorial/src/com/bedrockminer/tutorial/CommonProxy.java
@@ -2,10 +2,12 @@ package com.bedrockminer.tutorial;
 
 import com.bedrockminer.tutorial.block.ModBlocks;
 import com.bedrockminer.tutorial.item.ModItems;
+import com.bedrockminer.tutorial.world.ModWorldGen;
 
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
 
 public class CommonProxy {
 
@@ -15,7 +17,7 @@ public class CommonProxy {
 	}
 
 	public void init(FMLInitializationEvent e) {
-
+		GameRegistry.registerWorldGenerator(new ModWorldGen(), 0);
 	}
 
 	public void postInit(FMLPostInitializationEvent e) {
